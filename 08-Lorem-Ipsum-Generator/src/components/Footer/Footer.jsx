72d296c0747f8018679b271a28c297faa5c6fac8
SameerJS6 <sameersingh1234pok@gmail.com>
2023-03-12 23:23:07 +0530

    Footer Added

---
+++ 08-Lorem-Ipsum-Generator/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-center text-xs sm:text-sm font-medium text-primary mt-8">
+        <h3>&#169; Dummy Text Generator - 2023</h3>
+        <span>Privacy Policy</span>
+      </footer>
+    </>
+  );
+}
