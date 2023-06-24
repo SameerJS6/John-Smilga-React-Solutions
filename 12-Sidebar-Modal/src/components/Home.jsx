be833b8ad6621ae59765ea8f2b14c3959dc195e5
SameerJS6 <sameersingh1234pok@gmail.com>
2023-06-24 21:28:37 +0530

    Ripple Color Added

--- 12-Sidebar-Modal/src/components/Home.jsx
+++ 12-Sidebar-Modal/src/components/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
           <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
         </svg>
       </button>
-      <Button text={"Show Modal"} />
+      <Button customeColor="rgba(255, 255, 255, .3)" text={"Show Modal"} />
     </div>
   );
 }
