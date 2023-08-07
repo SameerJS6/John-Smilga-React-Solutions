bc0c96aecaa2ccddc1987fbe3ad93385ee81b828
SameerJS6 <sameersingh1234pok@gmail.com>
2023-08-07 20:34:24 +0530

    Data Added

---
+++ 14-Cart/src/data.jsx
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "iPhone 14 Pro",
+    price: 999.99,
+    img: "https://media.binglee.com.au/e/9/4/5/e945c8101e92df0b7de7b8811632959afb921a63_Apple_MQ9U3ZP_A_iPhone_Hero_3.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Google Pixel 7 Pro",
+    price: 899.99,
+    img: "https://th.bing.com/th/id/OIP.MnRdZczmD9f74KUc55OfdgHaHa?pid=ImgDet&w=500&h=500&rs=1",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Samsung Galaxy S23 Ultra",
+    price: 1099.99,
+    img: "https://media.pakprice.pk/288/otRdSrGmyr0jk9jpGFsDSevTiJBtoaK81UGAljcV.jpeg",
+    amount: 1,
+  },
+];
