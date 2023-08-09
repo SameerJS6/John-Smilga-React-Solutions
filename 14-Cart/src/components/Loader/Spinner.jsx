5fee658b44835a50c67c3c2b6233c90cabec7237
SameerJS6 <sameersingh1234pok@gmail.com>
2023-08-09 17:57:30 +0530

    Minor Changes in Spinner

--- 14-Cart/src/components/Loader/Spinner.jsx
+++ 14-Cart/src/components/Loader/Spinner.jsx
@@ -1,9 +1,5 @@
 import "./loader.css";
 
 export default function Spinner() {
-  return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
-      <div className="pulse"></div>
-    </div>
-  );
+  return <div className="pulse"></div>;
 }
