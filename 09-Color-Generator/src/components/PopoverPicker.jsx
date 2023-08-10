c5b36db455a80a4e3bffefe24278a69be50d2749
SameerJS6 <sameersingh1234pok@gmail.com>
2023-08-10 18:34:19 +0530

    useClickOutside Hook Added

--- 09-Color-Generator/src/components/PopoverPicker.jsx
+++ 09-Color-Generator/src/components/PopoverPicker.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { HexColorPicker } from "react-colorful";
+import useClickOutside from "../hook/useClickOutside";
 
 export default function PopoverPicker({ color, onChange }) {
-  const [isOpen, toggle] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  let PickerRef = useRef();
+
+  useClickOutside(PickerRef, () => setIsOpen(false));
   return (
     <div className="picker z-10">
       <div
         className="swatch"
         style={{ backgroundColor: color }}
-        onClick={() => toggle(!isOpen)}
+        onClick={() => setIsOpen(!isOpen)}
       />
 
       {isOpen && (
-        <div className="popover">
+        <div ref={PickerRef} className="popover">
           <HexColorPicker color={color} onChange={onChange} />
         </div>
       )}
