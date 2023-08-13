7451bcfc9efbc4f1fe96dfda3541ed3ed5166340
SameerJS6 <sameersingh1234pok@gmail.com>
2023-08-13 22:32:20 +0530

    Routes Added

--- 15-Siplore/src/App.tsx
+++ 15-Siplore/src/App.tsx
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// Import Components
+import Navbar from "./components/Navbar";
+// Import Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Errorpage from "./pages/Errorpage";
+import Cocktail from "./pages/Cocktail";
+
 type Props = {};
 
 export default function App({}: Props) {
   return (
     <>
-      <div>App</div>
+      <Router>
+        <Navbar />
+        {/* <Drawer/> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cocktail/:id" element={<Cocktail />} />
+          <Route path="*" element={<Errorpage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
