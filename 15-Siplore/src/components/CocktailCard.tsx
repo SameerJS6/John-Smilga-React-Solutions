58a3105b8e5468b932f51bf9b2b758da277f6f5f
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-20 21:58:03 +0530

    Cocktails Fetched

---
+++ 15-Siplore/src/components/CocktailCard.tsx
@@ -0,0 +1,65 @@
+import Tooltip from "../Elements/Tooltip";
+import CustomLink from "../Elements/CustomLink";
+
+type CocktailCardProps = {
+  id: number;
+  DrinkName: string;
+  Alcoholic: string;
+  DrinkImage: string;
+  Instructions: string;
+  Category: string;
+};
+
+export default function CocktailCard({
+  id,
+  DrinkImage,
+  DrinkName,
+  Alcoholic,
+  Category,
+}: CocktailCardProps) {
+  return (
+    <article className="slide-top card max-[400px]:card-compact w-full bg-base-200 shadow-xl rounded-3xl active:rounded-[2.5rem] active:scale-[99%] active:-translate-y-1 transition-all">
+      <figure>
+        <img
+          src={DrinkImage}
+          className="aspect-video object-cover"
+          alt={DrinkName + " Image"}
+        />
+      </figure>
+      <div className="card-body gap-2">
+        <div className="flex justify-between items-center gap-4">
+          <div className="space-y-2">
+            <h2 className="card-title text-2xl xl:text-3xl ">{DrinkName}</h2>
+            <p className="text-sm rounded-full bg-base-300 px-2 tracking-wide font-medium w-fit -ml-0.5">
+              {Category}
+            </p>
+          </div>
+          <Tooltip toolTipText={Alcoholic}>
+            <div
+              className={`border-2 w-fit p-1 ${
+                Alcoholic === "Alcoholic"
+                  ? "border-error"
+                  : "border-success"
+              }`}
+            >
+              <div
+                className={`w-2 h-2  rounded-full ${
+                  Alcoholic === "Alcoholic" ? "bg-error" : "bg-success"
+                }`}
+              ></div>
+            </div>
+          </Tooltip>
+        </div>
+        {/* <p>{Instructions}</p> */}
+        <div className="card-actions justify-start mt-2">
+          <CustomLink
+            className="capitalize rounded-3xl bg-neutral-focus  text-neutral-content text-base hover:bg-neutral active:rounded-lg transition-all btn-wide mx-auto"
+            pathTo={`/cocktail/${id}`}
+          >
+            Click Me
+          </CustomLink>
+        </div>
+      </div>
+    </article>
+  );
+}
--- 15-Siplore/src/components/CocktailGrid.tsx
+++ 15-Siplore/src/components/CocktailGrid.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { useGlobalContext } from "../context/Context";
 import Loader from "./Loader";
+import CocktailCard from "./CocktailCard";
 
 type Props = {};
 
@@ -8,7 +8,23 @@ export default function CocktailGrid({}: Props) {
   const { isLoading, cocktails } = useGlobalContext();
 
   if (isLoading) return <Loader />;
-  if (cocktails.length < 1) return <h1>No Cocktail Matches Your Search</h1>;
+  if (cocktails.length < 1)
+    return (
+      <h1 className="tracking-in font-medium text-center text-2xl sm:text-3xl lg:text-4xl xl:text-5xl px-4 text-error">
+        No Cocktail Matches Your Search
+      </h1>
+    );
 
-  return <div>CocktailGrid</div>;
+  return (
+    <section className="space-y-20 p-4 sm:px-6 lg:px-10 xl:px-12">
+      <div className="text-5xl font-medium text-center tracking-in">
+        <h1>Cocktails</h1>
+      </div>
+      <div className="grid gap-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {cocktails.map((drink) => {
+          return <CocktailCard key={drink.id} {...drink} />;
+        })}
+      </div>
+    </section>
+  );
 }
