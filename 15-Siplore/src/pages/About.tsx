c8c67ec80dcdc63e592f5255c3255ffd7327bbd8
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-21 23:11:58 +0530

    Background Pattern Added

--- 15-Siplore/src/pages/About.tsx
+++ 15-Siplore/src/pages/About.tsx
@@ -1,10 +1,10 @@
-// import React from 'react'
+import BlurPattern from "../components/BlurPattern";
 
 type Props = {};
 
 export default function About({}: Props) {
   return (
-    <main className="space-y-8 text-center py-24 lg:py-32 relative">
+    <main className="space-y-8 isolate text-center py-24 lg:py-32 relative">
       <div className="space-y-6 ">
         <h3 className="text-3xl capitalize font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
           Siplore - Elevate your sip
@@ -16,9 +16,17 @@ export default function About({}: Props) {
         </p>
       </div>
 
-      <div className="absolute top-1/2 left-1/2 -translate-y-[40%] -translate-x-[50%] max-w-5xl w-full blur-3xl  h-full -z-10">
-        <div className="path w-full h-full bg-yellow-500"></div>
-      </div>
+      <BlurPattern
+        containerClassName="-top-40 sm:-top-80"
+        elementClassName="left-[calc(50%-11rem)] bg-gradient-to-tr from-secondary to-accent sm:left-[calc(50%-30rem)]"
+        clipingPath="polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+      />
+
+      <BlurPattern
+        containerClassName="top-[calc(100%-13rem)] sm:top-[calc(100%-30rem)]"
+        elementClassName="left-[calc(50%+3rem)] bg-gradient-to-tr from-primary to-secondary sm:left-[calc(50%+36rem)]"
+        clipingPath="polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+      />
     </main>
   );
 }
