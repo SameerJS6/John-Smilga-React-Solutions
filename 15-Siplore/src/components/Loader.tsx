2ab3fd2d141a96abaea3f6d575692a8b192fb1ca
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-22 21:54:45 +0530

    Changed Reusable component

--- 15-Siplore/src/components/Loader.tsx
+++ 15-Siplore/src/components/Loader.tsx
@@ -1,3 +1,16 @@
-export default function Loader() {
-  return <span className="grid loading loading-dots loading-lg lg:w-14 mx-auto pt-36 xl:pt-40"></span>;
+import { twMerge } from "tailwind-merge";
+
+type LoaderProp = {
+  LoaderType: "loading-dots" | "loading-spinner" | "loading-ring";
+  className?: string;
+};
+export default function Loader({ LoaderType, className }: LoaderProp) {
+  return (
+    <span
+      className={twMerge(
+        `grid loading ${LoaderType} loading-lg lg:w-14 mx-auto pt-36 xl:pt-40`,
+        className
+      )}
+    ></span>
+  );
 }
