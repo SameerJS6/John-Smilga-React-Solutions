b80f419dbcdeb154a92b712545c438bd70a632e4
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-25 18:12:48 +0530

    Theme Switcher Done

--- 15-Siplore/src/components/Header/Themes.tsx
+++ 15-Siplore/src/components/Header/Themes.tsx
@@ -1,13 +1,30 @@
 import useRipple from "use-ripple-hook";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import useClickOutside from "../../hook/useClickOutside";
+import { themeChange } from "theme-change";
 type Props = {};
 
-// const Themes: string[] = ['light', "dark", "cupcake", "lofi", "black"]
+const ThemesOptions: string[] = [
+  "light",
+  "night",
+  "cupcake",
+  "lofi",
+  "black",
+  "luxury",
+  "wireframe",
+  "retro",
+  "pastel",
+  "valentine",
+];
 
 export default function Themes({}: Props) {
+  const StoredTheme: string | null = localStorage.getItem("theme");
+
   const [ripple, event] = useRipple();
   const [isActive, setIsActive] = useState<boolean>(false);
+  const [activeTheme, setActiveTheme] = useState<string>(
+    StoredTheme ? StoredTheme : ""
+  );
 
   let DropDownRef: React.RefObject<HTMLDetailsElement> = useRef(null);
 
@@ -19,8 +36,16 @@ export default function Themes({}: Props) {
   };
 
   useClickOutside(DropDownRef, closeDropDown);
+
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
   return (
-    <details ref={DropDownRef} className="dropdown max-lg:ml-6">
+    <details
+      ref={DropDownRef}
+      className="dropdown max-lg:dropdown-end max-lg:ml-6"
+    >
       <summary
         ref={ripple}
         onMouseDown={event}
@@ -45,40 +70,48 @@ export default function Themes({}: Props) {
         </svg>
         Themes
       </summary>
-      <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-200 rounded-lg max-w-md">
-        <li>
-          <button className="bg-base-100 rounded-md capitalize text-base font-medium">
-            <div className="flex items-center gap-5 justify-between">
-              {/* Active or Not Sign Container  */}
-              <div className="flex items-center gap-1.5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
+      <ul className="p-2 flex-nowrap shadow menu dropdown-content space-y-4 z-[1] bg-base-200 max-h-[400px] overflow-y-scroll rounded-lg w-56">
+        {ThemesOptions.map((theme, index) => {
+          return (
+            <button
+              key={index}
+              data-theme={theme}
+              data-set-theme={theme}
+              onClick={() => setActiveTheme(theme)}
+              className={`btn rounded-md capitalize text-base font-medium w-full theme ${
+                theme === activeTheme ? "btn-active" : ""
+              }`}
+            >
+              <div className="flex items-center gap-5 justify-between w-full">
+                {/* Active or Not Sign Container  */}
+                <div className="flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`w-3 h-3 ${
+                      theme === activeTheme ? "visible" : "invisible"
+                    }`}
+                  >
+                    <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
+                  </svg>
 
-                <p>light</p>
+                  <p>{theme}</p>
+                </div>
+
+                {/* Theme Color Block  */}
+                <div className="flex gap-1 items-center">
+                  <span className="bg-primary rounded-full shadow w-2 h-4"></span>
+                  <span className="bg-secondary rounded-full shadow w-2 h-4"></span>
+                  <span className="bg-accent rounded-full shadow w-2 h-4"></span>
+                  <span className="bg-neutral rounded-full shadow w-2 h-4"></span>
+                </div>
               </div>
-              
-              {/* Theme Color Block  */}
-              <div className="flex gap-1 items-center">
-                <span className="bg-primary rounded-full shadow w-2 h-4"></span>
-                <span className="bg-secondary rounded-full shadow w-2 h-4"></span>
-                <span className="bg-accent rounded-full shadow w-2 h-4"></span>
-                <span className="bg-neutral rounded-full shadow w-2 h-4"></span>
-              </div>
-            </div>
-          </button>
-        </li>
+            </button>
+          );
+        })}
       </ul>
     </details>
   );
