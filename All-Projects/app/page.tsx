ca9d081499437d8777be24f081c57d96289f7404
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-06 23:07:04 +0530

    All Projects Commited

---
+++ All-Projects/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        Hello Next Js, this is my first time creating a serverless api in next
+        js
+      </div>
+    </>
+  );
+}
