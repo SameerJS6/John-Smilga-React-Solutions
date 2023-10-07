81fd2be67c53584766c0376250d61239612a1080
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-07 22:52:30 +0530

    data updated

---
+++ All-Projects/data.ts
@@ -0,0 +1,77 @@
+export type projectsType = {
+  id: number;
+  url: string;
+  projectTitle: string;
+};
+
+const projectsData: projectsType[] = [
+  {
+    id: 1,
+    url: "https://birthday-reminder-singh.netlify.app/",
+    projectTitle: "birthday reminder",
+  },
+  { id: 2, url: "https://tours-singh.netlify.app/", projectTitle: "tours" },
+  {
+    id: 3,
+    url: "https://reviews-singh.netlify.app/",
+    projectTitle: "reviews",
+  },
+  {
+    id: 4,
+    url: "https://accordion-singh.netlify.app/",
+    projectTitle: "accordion",
+  },
+  {
+    id: 5,
+    url: "https://menu-singh.netlify.app/",
+    projectTitle: "menu",
+  },
+  {
+    id: 6,
+    url: "https://tabs-singh.netlify.app/",
+    projectTitle: "tabs",
+  },
+  {
+    id: 7,
+    url: "https://slider-singh.netlify.app/",
+    projectTitle: "slider",
+  },
+  {
+    id: 8,
+    url: "https://dummy-text-generator-singh.netlify.app/",
+    projectTitle: "dummy text generator",
+  },
+  {
+    id: 9,
+    url: "https://color-generator-singh.netlify.app/",
+    projectTitle: "color generator",
+  },
+  {
+    id: 10,
+    url: "https://grocery-bud-singh.netlify.app/",
+    projectTitle: "grocery bud",
+  },
+  { id: 11, url: "https://navbar1-singh.netlify.app/", projectTitle: "navbar" },
+  {
+    id: 12,
+    url: "https://sidebar-modal-singh.netlify.app/",
+    projectTitle: "sidebar & modal",
+  },
+  {
+    id: 13,
+    url: "https://stripe-submenu-singh.netlify.app/",
+    projectTitle: "stripe menu",
+  },
+  {
+    id: 14,
+    url: "https://cart-singh.netlify.app/",
+    projectTitle: "shopping cart",
+  },
+  {
+    id: 15,
+    url: "https://siplore.netlify.app/",
+    projectTitle: "siplore",
+  },
+];
+
+export default projectsData;
