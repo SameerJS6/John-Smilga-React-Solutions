d5f2d4a9d09deef6e4403b84c495d7e3ea346b20
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-08 23:07:03 +0530

    Project card updated

--- All-Projects/components/ProjectCard.tsx
+++ All-Projects/components/ProjectCard.tsx
@@ -1,30 +1,27 @@
 import React from "react";
 import Image from "next/image";
-import Mockup from "@/app/544shots_so.jpg";
-
-type ProjectCardProps = {
-  id: number;
-  url: string;
-  projectTitle: string;
-};
+import { projectsType } from "@/data";
 
 export default function ProjectCard({
   id,
   url,
+  mainImg,
   projectTitle,
-}: ProjectCardProps) {
+}: projectsType) {
   return (
     <a
       key={id}
       href={url}
       target="_blank"
-      className="relative rounded-3xl xl:rounded-[2rem] bg-zinc-900 overlay group active:rounded-[2rem] xl:active:rounded-[3rem] transition-all duration-300 ease-in-out"
+      className="relative rounded-3xl xl:rounded-[2rem] bg-zinc-900 overlay group active:rounded-[2rem] xl:active:rounded-[3rem] transition-all duration-300 ease-in-out self-start "
     >
       <div className="relative">
         <Image
-          src={Mockup}
+          src={mainImg}
+          width={450}
+          height={200}
           alt={projectTitle + " Mockups"}
-          className="rounded-3xl xl:rounded-[2rem] group-active:rounded-[2rem] xl:group-active:rounded-[3rem] transition-all duration-300 ease-in-out"
+          className="rounded-3xl xl:rounded-[2rem] group-active:rounded-[2rem] xl:group-active:rounded-[3rem] transition-all duration-300 ease-in-out w-full"
         />
         <div className="z-10 absolute left-4 bottom-4 xl:bottom-6 xl:left-6 group-hover:opacity-100 opacity-0 translate-y-1 group-hover:translate-y-0 group-focus-visible:opacity-100 group-focus-visible:translate-y-0 transition-all duration-300 ease-in-out">
           <h1 className="capitalize text-2xl lg:text-3xl font-semibold tracking-tight ">
