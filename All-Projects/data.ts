903dc33166a4209b4cab21b45dbc465917cf36cd
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-08 23:06:39 +0530

    projects data updated

--- All-Projects/data.ts
+++ All-Projects/data.ts
@@ -1,6 +1,8 @@
 export type projectsType = {
   id: number;
   url: string;
+  mainImg: string;
+  hoverImg: string;
   projectTitle: string;
 };
 
@@ -8,68 +10,136 @@ const projectsData: projectsType[] = [
   {
     id: 1,
     url: "https://birthday-reminder-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHd6EKtdH741OiBGrK4tXYbCmDs7ZJYqLLdzMSQn05z3IPYVqD6LJ_6HgEbRQFKyigjdcOy-gv0cFucSCLHjfeWqYcEaflFxBptd67Hn3OaoGwnINBHTTjA1ZTzYHr3I3lHwAPAB-jRWHNIaFEoiUR-S=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHd7MUbfEC9-Ach4gBgkRFvJWTtp6YLVU6ezPvEJhlgp_dPvZiQUS-ujsHDdG7L3_gIMuWvvlN0PLpdaa6L8FcvzjUQEDuHERCa2By9n8tIuDoHGZRGyQTBhbZLomCyuWMAineXfoSHAKd8ZBvqHyM3d=w908-h681-s-no",
     projectTitle: "birthday reminder",
   },
-  { id: 2, url: "https://tours-singh.netlify.app/", projectTitle: "tours" },
+  {
+    id: 2,
+    url: "https://tours-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHefZ5EOEdNUzF9hoEeXrvaUmAWUae3XOkbNlCO5AMWyIMvIBrvUtX4DJvmWVZMXk2IwYd1rfjuJbrBNLtHFo9kFiQSDkTNymlnNVndbIzUwta2I2fZcG5QD-oBuYJilAOg9wq0wGKsyKpnLIU4KyCAt=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHe3jaHW53Hyrt8G9keo4gCJ-FL4385ZNqzwKnAap-oFNQdomBeVb8ByM7hV3XjS2Hq_jlrPP5sFE54mmdgUngU_aBRmNCXPuyGkYUeTO5BZFXm3lb3Ff0DIeRGEmzamou1JhXqW0nQiPomWCB3_MHMj=w908-h681-s-no",
+    projectTitle: "tours",
+  },
   {
     id: 3,
     url: "https://reviews-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdAPaa-dTF5-GurxdnMGWPQr_LN8l5MMh0I9bOYuVOXoc3HyxF2nzVfTK7mfmr5A5hKvIMyvMgVeg1A7bax1AFHsHD4n3hNiKXQA8ogx2-H0P4zCvnVNqo9hc5bPi7QOr8a6ScZWMNV8kIojg1U_l1M=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHeZ_cfJhgHWuu26VUVEo1uCC2YQ6NqBusgj5W3j1raPCsaT7loyyZPdl3KdR1SSH_BujjCtujg63EJUjisQgbi4YXBnz0kLbzilBrVQsoNxA-P1kQxw6AD6GJvLLEV7XPVRFs257e3JWqwZZD9KSh4p=w908-h681-s-no",
     projectTitle: "reviews",
   },
   {
     id: 4,
     url: "https://accordion-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHd5WpeoyVihZuMb2ZHUBrkaC7AqLBYk-ZXc2mlcneZfaagbPuaG-wFWKJd6lETyOELDsAjdXAnUDji_XvOvveNj5OjocE_nIhheZahs83p_oc3E_l4zmKw-9TpjXbIjeRNB1jGkNUzNOYv7Fqt3hRHz=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHcxNIbWmt51XSeMjz3KH5XQaPzudnJ0G0fAYe-sbHZrdbWBcHN-DC5HKh3UkW3iN3DcLI4H8xWbUxWivZsaCK1rU5y8JdWfET9KWIMctiIZFjbtslEuSPY7gUC_iZKZJMuZ3kXtrkuEuQVcKlsXklhX=w908-h681-s-no",
     projectTitle: "accordion",
   },
   {
     id: 5,
     url: "https://menu-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHd7QVhOGk9ODBUs5Ib7Mhr1iaVxkTUeGBiU92KJffFvn-Ip7iMEnDtg0WPjsiT9sMOC6b8HR1YJ8GvW61Ene9tbj111JAtYjhvVvKiB2cAPakSk-YYqgRuCk_8TfikCJTtyXttBb36HzTGpXrM2zyx3=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHcrhBtF0D5ZfMw-jSJRFHFpm1rGCNnImB-9bZbWIxXTscbx-6P_-v7g92NCui3xMYNiTHKQnyRZYXg67fI6vhx788zM0_mHqC-54nPWG1ym2U8IJgQdVUKjNOa-Gbr8BuNyWWMe7GZMmPPZI2mNF2Jj=w908-h681-s-no",
     projectTitle: "menu",
   },
   {
     id: 6,
     url: "https://tabs-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHckmz2Cf4Bai1Rr2Urf66QsbW10OKlfXIvRWgIB_KKYzvUwbeAoS9o01oBsPwakXyynD4kRLkUtMJaWa7ZVWJIwDC1neD0ribvMHMkYZbkQwnN6JpEOQN3xj9GkpLC1yAgNuUJadSspPu2BDVDPz48K=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHeaFuqg6UPwBsSKIV2UWoVzlMpSk-RTJx3HSSh0-gUXK3sUl4GCXVlbQij361Y05_Zcr3iycxb84JnShzJCV0hAfsz69FRP62-djw-siq3LNpxdPIGPq4ADcJrOSDkaDfPo_IQxmSyWMTdKI0aTxxYw=w908-h681-s-no",
     projectTitle: "tabs",
   },
   {
     id: 7,
     url: "https://slider-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHc0BHV9QOWV_qShOxmnIRd_GUmtRAic9w44-ZwcAFNINKCzUz4TFBnrM6ZnxNFJpKY0LXMFPZXDMkeWM9xYBhF4ZLbO6ZGciU07ULRTCPXvhSAd5KiIC9rXzPqjWTXaNSOVIys1NHsnwvENhxxbfqiW=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHfuOKqZX0Gs6-CJKvyDOcgX-aSNj7UzoPxQYBurI3Ms0Sgh_VnDa2OHKDToIYqJZ8b927LImFdYseFEK5J_NCDBXtX2ycXvEYqMvAKQCoKlGpFO4VsLgbctcqvlQzoJespx5EWdlbOAnEYEMQ-w_XzF=w908-h681-s-no",
     projectTitle: "slider",
   },
   {
     id: 8,
     url: "https://dummy-text-generator-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdrYsR6KBRFkCDZq4Ytrcl5Im3HRyxpm1xQjFtrtIvULi2tLWu_WYQxtgr4AyV13m1nueMDj5tZl104zW7cPmMQxw7OBgT7rjaDQzuIl-4Nvg5M5mtewKRonJVFlmHXt-qoow08S4M7L6JHOob3q8kh=w681-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdzMU4eO2q01C0wTDqWMMMEzHlG5vpaInhe8DYJ4wrLdVW5R2rldAqkZ0OOU32ceTWm2GLksrNPky_UwOoWuXkb8peg-NXe7TnlbML4sN7IqQjt1Otqkk9TWntLy7DSOldwS_wcE5K9DNxTK7rn2Mtu=w681-h681-s-no",
     projectTitle: "dummy text generator",
   },
   {
     id: 9,
     url: "https://color-generator-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHfQlgDlMTEWbbSS9YtNg-a8qQB1u1wjswv7SOugVRhe2mbRhDufmKhBndI--L7Ba4oC8bd-bAJ_dpH-NM-VaU1yHJbVcEaVp873renLO4UgAecJmqtFOpt2RtH-p-llqG_rXdYJ7fHR2iZMbgbsYrn_=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHeYNACTxO1g4OIFHw61wj0oW4P-FuBgMTp8a5DC-sS59ZzSFfbZ2KUxqmhBNZsPMik5OYSri6zQybp34HWbbFcEFLXxkjozZtrcNYNbw7XJmBowXCXRve3I_-BVjSTSXSophS8FyHKc8RiwOxa1VurV=w908-h681-s-no",
     projectTitle: "color generator",
   },
   {
     id: 10,
     url: "https://grocery-bud-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdrz8AKMPF10rRA1l_HxZ77BZd0GoqCwh2b5LJXsFAopjVx6YS80AL3tvC26rykc1X1RdvBYgegk01aOqiubVlGCafsbZOSGC98ffiX9TijVFN5cQDh5kKv3bZUmQBabX9mV7lRCBPnqFb3JQMT2uRj=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHcFJrwPu4n1raaKk1NgIpjY1o-KcUK0YHHWgE-RuNwQ9Zs2XqOxftltjEHslFSJ58w5ICyrH_gmdIgTUTU0VCGnlq0tZ3L6l1VsjUuRgP24nclle7rUFByDC6906Iyo7IhTh9P7UZ_U-jFeHabXA_Td=w908-h681-s-no",
     projectTitle: "grocery bud",
   },
-  { id: 11, url: "https://navbar1-singh.netlify.app/", projectTitle: "navbar" },
+  {
+    id: 11,
+    url: "https://navbar1-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHd5AmPyemkvi_QMmJtIKXKjBJYXE8gPXEy9GCFdNdl0D_BFn1snO3meHCc-T-KxK8PXwYuHqFysvZJ_Eirdj-0POFzoIYT2Q8YNEUnLRHIugDYE5UJaxh4e_GbEDziDiZ_Ha_F_yJauMJpvlFXz_KAz=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHekJf8YSWiL3ZsFxNlphBlJo5ZDDigJDLc8IsFxExXAlJBq56nHGzrRw4ZF-Hkg6BuHb_l2y9tzfnP6kURDX7uAQrclhHTITlozXbK5DqH4bke-dVtkCLb3oMfH8dGEwk1BAGzqKgxMlz6Y54sPHCbR=w908-h681-s-no",
+    projectTitle: "navbar",
+  },
   {
     id: 12,
     url: "https://sidebar-modal-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdqv2O3U70RhYJow09BVoAkB66vjpi1lo7jaa6y61tzMBB8obCaEdSHqgZHX9RGqIXAZ77sNg7NL-SBFWdw_hWz0fkXFlIGuKSVCsRJCkWAMRNgBqH3Ezt_ECMaOqD_wnNLRSFt_bD3-iSEntmciPIY=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHf1BF7KqAGtmr0XLcj9a1erHMtgj5uxlfmz0KCjPfH_sbmqAQ_p5trfSDB6UTT19RYAqh--eNJjUVnJmqyNUATvFjOS0znEKOOmBxO0aX4Nt_UXglOMw7Y45uGRvgu7HQN7YohabMEi0KRWfh-KmINF=w908-h681-s-no",
     projectTitle: "sidebar & modal",
   },
   {
     id: 13,
     url: "https://stripe-submenu-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdEvmMLeufUAjVDJR2sAH9omJL1m9SXW_2q6HiXx-3hFOSjfl1GpqPVi0WrcstNCfOcvnnl_pb96XoBnZoSCtq7YaXNF3kp3z0i2GXn6xb21Ik3fy7rDa_BZSgL8rsrWejXMTi-nGOmUdC67ycit0sy=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdJBcgMNwOX67qL5FmUQN6ofbmR2QpjXDsAeNYWfw9-_H2YsBWsO5A1LjjVkzVKoFgqbMqJ2ZLQr722luynMpUbfE-sguX_mpK_wN9sOcuoC1mp-lfA1nkWlFMplDSHFXvTH7XSTneJi-RowimBPDWb=w908-h681-s-no",
     projectTitle: "stripe menu",
   },
   {
     id: 14,
     url: "https://cart-singh.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHevguXubGdNiB2RkKB6Ir6xy6BKaW3VXeSM5w9QRP-wKiFFnzB1aoPVngDQ4718F0khRyNkNrafE61VMmmkQ7-0orZmPzNSZzWNMD6KNnyyBZXIF1C_NsHE0hQvl-zVaYfUo1dSEXCHzfgHWVgkDMGc=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHdiNZifVlOENy0CgldbdBiVNzqGWDjDQvtHLwSYH1wCNdtdvQIV9mv06h7SJ57Mf5_9n-Os7896JA9JZCaxb3fqSg3K65Ax6Y6_jlbVnhjkHOdkI6Uy9ko88cqBY-auUJK6mIOH7ZtO5B0FaleO9iiK=w908-h681-s-no",
     projectTitle: "shopping cart",
   },
   {
     id: 15,
     url: "https://siplore.netlify.app/",
+    mainImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHfk2rxWSF3ZfT2LYHYiF5cVw5sMmWqyW9L-rQxu4hyGcLyUwTM0PH6pQRb274E9YOrg26fs5_Hcnb85WVvzuysSUFIE6fAOoEHPqvZuKk1XqYN2Nr9mR_sPmnDOWK_CZaBjB16T-Vd6OA_ixR6WdBmh=w908-h681-s-no",
+    hoverImg:
+      "https://lh3.googleusercontent.com/pw/ADCreHe9OXkA5_yIJjTi8aAkMeF6GEk_SRXTsSN8ivmTZ86nDEYhKBNbGhjIKE8dzLFobwgR9OXKJmFVyIwTuB867nXMAvF5vR26sQperpohfQM0Uc24pJZCrIZQgsj76EdnLq-SbmLzkSjbHRvGumMg_XlR=w908-h681-s-no",
     projectTitle: "siplore",
   },
 ];
