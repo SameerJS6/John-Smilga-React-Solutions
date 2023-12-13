8399dc8c708fea8789f1865d421596c0977297c3
SameerJS6 <singh6.sameer@gmail.com>
2023-12-14 00:00:50 +0530

    style: add heading & scrollbar styles

--- All-Projects/app/page.tsx
+++ All-Projects/app/page.tsx
@@ -10,6 +10,11 @@ export default async function Home() {
   const projects: projectsType[] = await response.json();
   return (
     <>
+      <div className="p-4 xl:p-8 pb-3 xl:pb-3">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          John Smilga React Projects
+        </h1>
+      </div>
       <div className="p-4 xl:p-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => {
           return <ProjectCard key={project.id} {...project} />;
