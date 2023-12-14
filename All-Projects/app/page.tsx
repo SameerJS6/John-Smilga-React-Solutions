43cd5df5a8f93b382d966debf5ad53fa32a8aea5
SameerJS6 <singh6.sameer@gmail.com>
2023-12-14 13:12:18 +0530

    fix: data fetching from api.

--- All-Projects/app/page.tsx
+++ All-Projects/app/page.tsx
@@ -1,15 +1,16 @@
 import { projectsType } from "@/data";
 import ProjectCard from "@/components/ProjectCard";
 import Cursor from "@/components/Cursor";
-import projectsData from "@/data";
 
 export default async function Home() {
-  //   const response = await fetch("http://localhost:3000/api/projects", {
-  //     method: "GET",
-  //     cache: "no-cache",
-  //   });
-  //   const projects: projectsType[] = await response.json();
-  const projects: projectsType[] = projectsData;
+  const response = await fetch(
+    "https://john-smilga-react-solutions.vercel.app/api/projects",
+    {
+      method: "GET",
+      cache: "no-cache",
+    }
+  );
+  const projects: projectsType[] = await response.json();
   return (
     <>
       <div className="p-4 xl:p-8 pb-3 xl:pb-3">
@@ -19,7 +20,7 @@ export default async function Home() {
       </div>
 
       <div className="p-4 xl:p-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project) => {
+        {projects.map((project, index) => {
           return <ProjectCard key={project.id} {...project} />;
         })}
       </div>
