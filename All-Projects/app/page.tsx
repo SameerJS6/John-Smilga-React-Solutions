09a67e10304a982c89954b6a339efb25243af755
SameerJS6 <singh6.sameer@gmail.com>
2023-12-14 12:47:14 +0530

    fix: project data fetching removed

--- All-Projects/app/page.tsx
+++ All-Projects/app/page.tsx
@@ -1,13 +1,15 @@
 import { projectsType } from "@/data";
 import ProjectCard from "@/components/ProjectCard";
 import Cursor from "@/components/Cursor";
+import projectsData from "@/data";
 
 export default async function Home() {
-  const response = await fetch("https://johnsmilga.vercel.app/api/projects", {
-    method: "GET",
-    cache: "no-cache",
-  });
-  const projects: projectsType[] = await response.json();
+  //   const response = await fetch("http://localhost:3000/api/projects", {
+  //     method: "GET",
+  //     cache: "no-cache",
+  //   });
+  //   const projects: projectsType[] = await response.json();
+  const projects: projectsType[] = projectsData;
   return (
     <>
       <div className="p-4 xl:p-8 pb-3 xl:pb-3">
@@ -15,6 +17,7 @@ export default async function Home() {
           John Smilga React Projects
         </h1>
       </div>
+
       <div className="p-4 xl:p-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => {
           return <ProjectCard key={project.id} {...project} />;
